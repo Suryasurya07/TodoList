cb416796cc913670dc4125d728726045f895a497
Surya <suryams404@gmail.com>
2025-01-22 23:44:28 +0530

    update

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -9,7 +9,7 @@ const Quote = () => {
   useEffect(() => {
     const fetchQuote = async () => {
       try {
-        const response = await fetch('http://api.quotable.io/random');
+        const response = await fetch('https://api.quotable.io/random');
         
         if (!response.ok) {
           throw new Error('Failed to fetch quote');
